295918eef597f5993ed4abfa1021ef5da7d94fd9
mrshubhampandey1 <pandeyshubham4662>
2026-05-13 16:50:04 +0530

    Cached Functionality added in Autocomplete

--- src/Pages/AutoComplete.jsx
+++ src/Pages/AutoComplete.jsx
@@ -4,15 +4,24 @@ function AutoComplete() {
   const [input, setInput] = useState("");
   const [show, setShow] = useState(false);
   const [res, setRes] = useState([]);
+  const [cache, setCache] = useState({});
 
   //API
   const API = "https://dummyjson.com/recipes/search?q=";
+  //   console.log("cahced", cache);
 
   //Fetching API data
   const fetchAPI = async () => {
+    //implementing logic for cache
+    if (cache[input]) {
+      console.log("Cached", input);
+      setRes(cache[input]);
+      return;
+    }
     let data = await fetch(API + input);
     let furnishedData = await data.json();
     setRes(furnishedData?.recipes);
+    setCache((prev) => ({ ...prev, [input]: furnishedData?.recipes }));
     console.log(furnishedData?.recipes);
   };
 
@@ -26,21 +35,28 @@ function AutoComplete() {
   }, [input]);
 
   return (
-    <div>
-      <input
-        type="text"
-        value={input}
-        onChange={(e) => setInput(e.target.value)}
-        onFocus={() => setShow(true)}
-        onBlur={() => setShow(false)}
-      />
-      {show && (
-        <div className="listDisplay">
-          {res.map((item) => (
-            <li key={item.id}>{item.name}</li>
-          ))}
-        </div>
-      )}
+    <div className="contianer">
+      <div>
+        <h1>Autocomplete Search Bar:</h1>
+      </div>
+      <div>
+        <input
+          type="text"
+          value={input}
+          onChange={(e) => setInput(e.target.value)}
+          onFocus={() => setShow(true)}
+          onBlur={() => setShow(false)}
+        />
+        {show && (
+          <div className="listDisplay">
+            {res.map((item) => (
+              <li key={item.id} className="lsitItems">
+                {item.name}
+              </li>
+            ))}
+          </div>
+        )}
+      </div>
     </div>
   );
 }
